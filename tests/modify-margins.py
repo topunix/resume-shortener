d5f9322e5b06d17c6a67ef28a87c338bb2dfed45
mixedconnections <mixed.connections2@gmail.com>
2020-03-18 15:51:39 -0400

    added sample code

--- tests/modify-margins.py
+++ tests/modify-margins.py
@@ -1,3 +1,21 @@
 '''
 Test for modifying margins
 '''
+import docx
+from docx.shared import Inches
+
+samples = './samples/valid/'
+
+#Open the document
+document = Document(args.inputFile)
+
+#changing the page margins
+sections = document.sections
+for section in sections:
+    section.top_margin = Cm(margin)
+    section.bottom_margin = Cm(margin)
+    section.left_margin = Cm(margin)
+    section.right_margin = Cm(margin)
+
+document.save(args.outputFile)
+
