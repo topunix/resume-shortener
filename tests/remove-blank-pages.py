c4cc0356579d7b4a6d11ce2379ad8dd9b193c7f5
mixedconnections <mixed.connections2@gmail.com>
2020-03-19 18:06:16 -0400

    added blank check

--- tests/remove-blank-pages.py
+++ tests/remove-blank-pages.py
@@ -1,3 +1,9 @@
 '''
 Test for removing blank pages
 '''
+import docx
+samples = "./samples/valid/"
+#doc = docx.Document(samples + 'resume.docx')
+doc = docx.Document("/c/docs/resume3.docx")
+p = doc.paragraphs[5]
+print(len(p.text)) # will be zero for blank lines
