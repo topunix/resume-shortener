b0e4af0c67964bdd20659b3f297d435c7ecacf40
mixedconnections <mixed.connections2@gmail.com>
2020-03-18 20:01:49 -0400

    added working code

--- tests/modify-fonts.py
+++ tests/modify-fonts.py
@@ -1,3 +1,24 @@
 '''
 Test for modifying font face and size
 '''
+import docx
+from docx.shared import Pt
+
+samples = './samples/valid/'
+
+# see: https://www.programcreek.com/python/example/89765/docx.Document
+# see: https://stackoverflow.com/questions/27884703/set-paragraph-font-in-python-docx
+#Open the document
+doc = docx.Document(samples + 'resume.docx')
+style = doc.styles['Normal']
+font = style.font
+font.name = 'Arial'
+font.size = Pt(10)
+
+#for p in doc.paragraphs:
+#    font = p.font
+#    font.name = 'Calibri'
+#    font.size = Pt(10.5)
+
+doc.save('/c/docs/resume2.docx')
+
