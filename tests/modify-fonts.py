c584f287872e75ea6739a341eec62d4149b6a389
mixedconnections <mixed.connections2@gmail.com>
2020-03-21 15:17:00 -0400

    completed tests

--- tests/modify-fonts.py
+++ tests/modify-fonts.py
@@ -1,11 +1,13 @@
 '''
 Test for modifying font face and size
 '''
+import os
 import docx
+import pytest
 from docx.shared import Pt
 
 samples = './samples/valid/'
-
+tmpdir = './samples/tmp/'
 # see: https://www.programcreek.com/python/example/89765/docx.Document
 # see: https://stackoverflow.com/questions/27884703/set-paragraph-font-in-python-docx
 #Open the document
@@ -14,11 +16,19 @@ style = doc.styles['Normal']
 font = style.font
 font.name = 'Arial'
 font.size = Pt(10)
+fontfile = tmpdir + 'fonts.docx'
+doc.save(fontfile)
+doc = docx.Document(fontfile)
+style = doc.styles['Normal']
+font = style.font
 
-#for p in doc.paragraphs:
-#    font = p.font
-#    font.name = 'Calibri'
-#    font.size = Pt(10.5)
+def test_font_name():
+    assert font.name == 'Arial'
 
-doc.save('/c/docs/resume2.docx')
+def test_font_size():
+    assert font.size == 127000
 
+try:
+    os.remove(fontfile)
+except OSError as e:  ## if failed, report it back to the user ##
+    print("Error: %s - %s." % (e.filename, e.strerror))
