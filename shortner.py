0f3354c5dd7aa112a2bf2fd9246887b35c76ea22
mixedconnections <mixed.connections2@gmail.com>
2020-04-17 15:19:05 -0400

    added functions

--- shortner.py
+++ shortner.py
@@ -1,82 +1,100 @@
 import os
 import re
 import sys
-import docx
+from docx import Document
 from docx.shared import Inches, Pt
 
 if len(sys.argv) != 2:
     sys.exit("Usage: python " + sys.argv[0] + " resume.docx")
 
-filename = sys.argv[1]
-
 # Test if file is a docx file.
 try:
-    docx.Document(filename)
+    filename = sys.argv[1]
+    Document(filename)
 except:
     sys.exit(filename + " is not a valid docx file.")
 
+doc = Document(filename)
 
-#
-# modify-fonts, modify-margins, delete-sections, remove-blank-pages
-#
-
-
-
-
-
-
-
-
+def modify_margins():
+    # Change the page margins
+    sections = doc.sections
+    for section in sections:
+        section.top_margin = Inches(.25)
+        section.bottom_margin = Inches(.25)
+        section.left_margin = Inches(.25)
+        section.right_margin = Inches(.25)
 
 
+def modify_fonts():
+    # Modify the font face and size
+    style = doc.styles['Normal']
+    font = style.font
+    font.name = 'Arial'
+    font.size = Pt(10)
+
+
+def delete_paragraph(paragraph):
+    if len(paragraph.text) > 0:
+        return
+    p = paragraph._element
+    p.getparent().remove(p)
+    p._p = p._element = None
+
+
+def delete_extra_spaces():
+    delete_ids = []
+    # From the front, find spaces
+    for idx,p in enumerate(doc.paragraphs):
+        if len(p.text) == 0:
+            delete_ids.append(idx)
+        else:
+            break
+
+    for id in delete_ids:
+        if id > 0:
+            p = doc.paragraphs[0]
+            if len(p.text) > 0:
+                break
+            else:
+                id = 0
+        p = doc.paragraphs[id]
+        delete_paragraph(p)
+
+    # From the back, find spaces
+    delete_ids = []
+    num_p = len(doc.paragraphs)
+    for idx,p in enumerate(reversed(doc.paragraphs)):
+        num_p -= 1
+        if len(p.text) == 0:
+            delete_ids.append(num_p)
+        else:
+            break
+
+    for id in delete_ids:
+        p = doc.paragraphs[id]
+        delete_paragraph(p)
 
 
 
+def delete_overused_phrases():
+    pattern = re.compile("references (available|upon)", re.IGNORECASE)
+    delete_id = None
+
+    for idx,p in enumerate(doc.paragraphs):
+        if pattern.search(p.text):
+            delete_id = idx
+
+    if delete_id is not None:
+        p = doc.paragraphs[delete_id]
+        delete_paragraph(p)
 
 
-'''
-from docx import Document
-from docx.shared import Inches
+# call functions
+modify_margins()
+modify_fonts()
+delete_extra_spaces()
+delete_overused_phrases()
 
-document = Document()
-
-document.add_heading('Document Title', 0)
-
-p = document.add_paragraph('A plain paragraph having some ')
-p.add_run('bold').bold = True
-p.add_run(' and some ')
-p.add_run('italic.').italic = True
-
-document.add_heading('Heading, level 1', level=1)
-document.add_paragraph('Intense quote', style='Intense Quote')
-
-document.add_paragraph(
-    'first item in unordered list', style='List Bullet'
-)
-document.add_paragraph(
-    'first item in ordered list', style='List Number'
-)
-
-#document.add_picture('monty-truth.png', width=Inches(1.25))
-
-records = (
-    (3, '101', 'Spam'),
-    (7, '422', 'Eggs'),
-    (4, '631', 'Spam, spam, eggs, and spam')
-)
-
-table = document.add_table(rows=1, cols=3)
-hdr_cells = table.rows[0].cells
-hdr_cells[0].text = 'Qty'
-hdr_cells[1].text = 'Id'
-hdr_cells[2].text = 'Desc'
-for qty, id, desc in records:
-    row_cells = table.add_row().cells
-    row_cells[0].text = str(qty)
-    row_cells[1].text = id
-    row_cells[2].text = desc
-
-document.add_page_break()
-
-document.save('demo.docx')
-'''
+# save file
+doc.save(filename + '-short')
